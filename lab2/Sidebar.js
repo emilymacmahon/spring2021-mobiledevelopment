517069a9e6b627d110f2107f4cb556e42695f59f
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-02-05 12:34:09 -0500

    Update Sidebar.js
    commenting, adding to responseHTML

--- lab2/Sidebar.js
+++ lab2/Sidebar.js
@@ -8,12 +8,23 @@ class Sidebar extends Component {
 
   render () {
    
+    // Setup the HTML string
     let responseHTML = '';
 
+    
+    // Loop through each item in array and create a list item
     this.props.menu.forEach( (item) => {
-      // TODO: Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li>
+      // Wrap items in an unordered list
+
+      // TODO: Add (concatenate) the value of each item to the variable responseHTML. 
+
+      // Each should be wrapped in <li></li>
+      
+      responseHTML += '<li>' + Sidebar + '</li>';
     });
 
+
+    
     return responseHTML;
   }
 }
